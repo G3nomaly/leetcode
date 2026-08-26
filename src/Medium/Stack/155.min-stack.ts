28880774b60da50f19528a84e49013826a7ab040
w.dobbenberg <wouter.dobbenberg@cgi.com>
2026-08-26 10:34:50 +0200

    155 complete

--- src/Medium/Stack/155.min-stack.ts
+++ src/Medium/Stack/155.min-stack.ts
@@ -57,24 +57,34 @@
 */
 
 class MinStack {
+    stack: Array<number>;
+    minStack: Array<number>;
+    
     constructor() {
-        
+        this.stack = [];
+        this.minStack = [];
     }
 
     push(value: number): void {
-        
+        this.stack.push(value)
+        if (this.minStack.length === 0) {
+            this.minStack.push(value)
+        } else {
+            this.minStack.push(Math.min(value, this.minStack[this.minStack.length - 1]!))
+        }
     }
 
     pop(): void {
-        
+        this.stack.pop()
+        this.minStack.pop()
     }
 
     top(): number {
-        
+        return this.stack[this.stack.length - 1] ?? NaN
     }
 
     getMin(): number {
-        
+        return this.minStack[this.minStack.length - 1] ?? NaN
     }
 }
 
